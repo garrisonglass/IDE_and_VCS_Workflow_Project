b29744bc7b85d84e72ebc1cea2f6b1523ff84b1d
garrisonglass <garrison.glass@gmail.com>
2026-01-13 20:01:08 -0500

    changed to "main"

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// This is a simple C++ program that prints
+// "Hello, World! to the console.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
