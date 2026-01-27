91e0634fbcdc8a0a933bdb7f01d3294cc0a3c4c3
garrisonglass <garrison.glass@gmail.com>
2026-01-26 19:17:52 -0500

    Added code per instructions

---
+++ IDE_and_VCS_Workflow_Project/main.cpp
@@ -0,0 +1,12 @@
+// This is a simple C++ program that prints 
+// "Hello World!" to the console.
+#include <iostream>
+
+using namespace std;
+
+int main()
+{// I realize the instruction said exactly but I like to put my open bracket here. Is that going to be an issue?
+    cout << "Hello World!" << endl;
+	return 0;
+}
+
